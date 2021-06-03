dd8e1cb04a1b64a48a355ac8a89fa3300857f78a
iRuxu <rx6@qq.com>
2021-06-03 21:51:49 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,5 @@
 <template>
     <div id="app">
-        <Header></Header>
-        <Breadcrumb
-            name="云端宏"
-            slug="macro"
-            root="/macro"
-            :publishEnable="true"
-            :adminEnable="true"
-            :feedbackEnable="true"
-            :crumbEnable="true"
-        >
-            <img slot="logo" svg-inline src="./assets/img/logo.svg" />
-        </Breadcrumb>
-        <LeftSidebar>left side</LeftSidebar>
-        <Main :withoutRight="false">
-            <div class="m-main">
-                <router-view />
-            </div>
-            <RightSidebar>right side</RightSidebar>
-            <Footer></Footer>
-        </Main>
     </div>
 </template>
 
--- src/service/axios.js
+++
@@ -1,3 +0,0 @@
-import { $helper, $cms, $next, $team } from "@jx3box/jx3box-common/js/https.js";
-
-export { $helper, $cms, $next, $team };
